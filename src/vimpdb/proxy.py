eb76abed3402a403637ae7486f692df8bba72ea3
Godefroid Chapelle <gotcha@bubblenet.be>
2010-04-18 16:14:51 +0200

    reorder code for better readability

--- src/vimpdb/proxy.py
+++ src/vimpdb/proxy.py
@@ -15,50 +15,10 @@ def getPackagePath(instance):
 
 
 class ProxyToVim(object):
-
-    def setupRemote(self):
-        if not self.isRemoteSetup():
-            filename = os.path.join(getPackagePath(self), "vimpdb.vim")
-            command = "<C-\><C-N>:source %s<CR>" % filename
-            self._send(command)
-
-    def getText(self, prompt):
-        self.setupRemote()
-        command = self._expr('PDB_get_command("%s")' % prompt)
-        return command
-
-    def showFeedback(self, feedback):
-        if not feedback:
-            return
-        feedback_list = feedback.splitlines()
-        self.setupRemote()
-        self._send(':call PDB_show_feedback(%s)<CR>' % repr(feedback_list))
-
-    def showFileAtLine(self, filename, lineno):
-        if os.path.exists(filename):
-            self._showFileAtLine(filename, lineno)
-
-    def _send(self, command):
-        return_code = call([PROGRAM, '--servername', SERVERNAME,
-                            '--remote-send', command])
-        if return_code:
-            raise RemoteUnavailable()
-        print "sent:", command
-
-    def _showFileAtLine(self, filename, lineno):
-        # Windows compatibility:
-        # Command-line does not play well with backslash in filename.
-        # So turn backslash to slash; Vim knows how to translate them back.
-        filename = filename.replace('\\', '/')
-        self.setupRemote()
-        self._send(':call PDB_show_file_at_line("%s", "%d")<CR>'
-            % (filename, lineno))
-
-    def _expr(self, expr):
-        print "expr:", expr
-        result = self._remote_expr(expr)
-        print "result:", result
-        return result
+    """
+    use subprocess to launch Vim instance that use clientserver mode
+    to communicate with Vim instance used for debugging.
+    """
 
     def _remote_expr(self, expr):
         p = Popen([PROGRAM, '--servername',
@@ -71,9 +31,54 @@ class ProxyToVim(object):
         output = child_stdout.read()
         return output.strip()
 
+    def _send(self, command):
+        return_code = call([PROGRAM, '--servername', SERVERNAME,
+                            '--remote-send', command])
+        if return_code:
+            raise RemoteUnavailable()
+        print "sent:", command
+
+    def setupRemote(self):
+        if not self.isRemoteSetup():
+            filename = os.path.join(getPackagePath(self), "vimpdb.vim")
+            command = "<C-\><C-N>:source %s<CR>" % filename
+            self._send(command)
+
     def isRemoteSetup(self):
         status = self._remote_expr("exists('*PDB_init')")
         return status == '1'
+
+    def showFeedback(self, feedback):
+        if not feedback:
+            return
+        feedback_list = feedback.splitlines()
+        self.setupRemote()
+        self._send(':call PDB_show_feedback(%s)<CR>' % repr(feedback_list))
+
+    def showFileAtLine(self, filename, lineno):
+        if os.path.exists(filename):
+            self._showFileAtLine(filename, lineno)
+
+    def _showFileAtLine(self, filename, lineno):
+        # Windows compatibility:
+        # Windows command-line does not play well with backslash in filename.
+        # So turn backslash to slash; Vim knows how to translate them back.
+        filename = filename.replace('\\', '/')
+        self.setupRemote()
+        self._send(':call PDB_show_file_at_line("%s", "%d")<CR>'
+            % (filename, lineno))
+
+    # code leftover from hacking
+    def getText(self, prompt):
+        self.setupRemote()
+        command = self._expr('PDB_get_command("%s")' % prompt)
+        return command
+
+    def _expr(self, expr):
+        print "expr:", expr
+        result = self._remote_expr(expr)
+        print "result:", result
+        return result
 
 
 class ProxyFromVim(object):
@@ -106,6 +111,7 @@ class RemoteUnavailable(Exception):
     pass
 
 
+# code leftover from hacking
 def eat_stdin(self):
     sys.stdout.write('-- Type Ctrl-D to continue --\n')
     sys.stdout.flush()
